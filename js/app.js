8913786b45e6580029821000babe9004e02bb778
Sascha Mick <Sascha Mick>
2016-06-13 19:25:04 +0200

    Responsive Header Bilder

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
-$(document).ready(function () {
+function kenBurns() {
     var width = $(document).width();
     if (width <= 600) {
         $('.ken-burns').attr("src","img/header_small.jpg");
     } else if (width <= 992) {
         $('.ken-burns').attr("src","img/header_medium.jpg");
+    } else {
+        $('.ken-burns').attr("src","img/header_large.jpg");
     }
     $('.ken-burns').width(width+100);
+}
+
+$(document).ready(function () {
+    kenBurns();
     
     $(".button-collapse").sideNav();
     $('.ken-burns').addClass('zoomed');
+});
+
+$(window).resize(function() {
+    kenBurns();
 });
