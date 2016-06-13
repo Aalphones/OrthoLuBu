8757fbf0fbe0fc6905b051d8141abc70fc472d72
Sascha Mick <Sascha Mick>
2016-06-13 19:19:21 +0200

    Ken Burns Efffekt für den Header hizufügen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
+    var width = $(document).width();
+    if (width <= 600) {
+        $('.ken-burns').attr("src","img/header_small.jpg");
+    } else if (width <= 992) {
+        $('.ken-burns').attr("src","img/header_medium.jpg");
+    }
+    $('.ken-burns').width(width+100);
+    
     $(".button-collapse").sideNav();
+    $('.ken-burns').addClass('zoomed');
 });
