4dc111a6c200248f19f37e423b8c60ef25decd96
Sascha Mick <Sascha Mick>
2016-06-13 20:22:29 +0200

    Ken Burns Effekt anpassen

--- js/app.js
+++ js/app.js
@@ -7,14 +7,33 @@ function kenBurns() {
     } else {
         $('.ken-burns').attr("src","img/header_large.jpg");
     }
-    $('.ken-burns').width(width+100);
+    $('.ken-burns').width(width+200);
+}
+
+function overlay() {
+    var nav = $('nav');
+    var scrollPosition = $(document).scrollTop();
+    console.log(scrollPosition / nav.offset().top);
+    if (scrollPosition != 0.0) {
+        if (scrollPosition > $('#page-header').height() - nav.height() ) {
+            $(nav).addClass("fixed");
+        } else {
+            $('.ken-burns').css("opacity", 1.0 - (scrollPosition/nav.offset().top));
+            $(nav).removeClass("fixed");
+        }
+    }
 }
 
 $(document).ready(function () {
     kenBurns();
+    overlay();
     
     $(".button-collapse").sideNav();
     $('.ken-burns').addClass('zoomed');
+    
+    $(this).scroll(function() {
+        overlay();
+    });
 });
 
 $(window).resize(function() {
